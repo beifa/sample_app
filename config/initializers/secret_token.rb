fd2481ba64fb71031cf95204c2c417783a211200
beifa <beifabeifa@mail.ru>
2013-09-15 18:46:48 +0000

    New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5b0898d764f841ec3f0184b0979c669a202e501e87dd09d9b930a125362d95f842e1e00e6407a00fa77896a9e58f7197f522e882948b8b1674d6d0af2bc7f321'
